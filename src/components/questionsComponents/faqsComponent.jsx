c9f23bb0f89b0cf961e3156553c687a2c7f2c12a
Pau Larrea <paularrea12@hotmail.com>
2021-05-12 18:19:02 +0200

    faqs hover desktop

--- src/components/questionsComponents/faqsComponent.jsx
+++ src/components/questionsComponents/faqsComponent.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import { faq_container, question } from "./faqs.module.scss";
 
 const FaqsComponent = () => {
-    return (
-        <div>
-           FAQs 
-        </div>
-    )
-}
+  return (
+    <div className={faq_container}>
+      <span>Quédate completamente tranquilo.</span>
+      <h2>Preguntas frecuentes</h2>
+      <div className={question}>
+        <h4>¿Cómo registro mis perros en la webapp?</h4>
+        <p>
+          Para registrar tus perros deberás ir a símbolo de configuración y
+          dirigirte al apartado “mis perros”. Desde ahí podrás borrar o añadir
+          perros, modificar sus datos y activar las placas que te falten.
+        </p>
+      </div>
+      <div className={question}>
+        <h4>¿Puedo localizar a mi perro a través de la webapp?</h4>
+        <p>
+          Para registrar tus perros deberás ir a símbolo de configuración y
+          dirigirte al apartado “mis perros”. Desde ahí podrás borrar o añadir
+          perros, modificar sus datos y activar las placas que te falten.
+        </p>
+      </div>
+      <div className={question}>
+        <h4>¿Puedo pagar una suscripción por el collar sin el pago inicial?</h4>
+        <p>
+          Para registrar tus perros deberás ir a símbolo de configuración y
+          dirigirte al apartado “mis perros”. Desde ahí podrás borrar o añadir
+          perros, modificar sus datos y activar las placas que te falten.
+        </p>
+      </div>
+      <div className={question}>
+        <h4>¿Cómo puedo desactivar las alertas de los perros perdidos?</h4>
+        <p>
+          Para registrar tus perros deberás ir a símbolo de configuración y
+          dirigirte al apartado “mis perros”. Desde ahí podrás borrar o añadir
+          perros, modificar sus datos y activar las placas que te falten.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default FaqsComponent
+export default FaqsComponent;
