ae99334236e58b0956b922b4932f3d78284de41f
Pau Larrea <paularrea12@hotmail.com>
2021-05-25 16:43:40 +0200

    fixedIcons

--- src/components/questionsComponents/mediaFixed.jsx
+++ src/components/questionsComponents/mediaFixed.jsx
@@ -9,42 +9,34 @@ import twitterIcon from "../../images/icons/twitter.png";
 const MediaFixed = () => {
   return (
     <div className={fixed_container}>
-      <div>
-        <a
-          href="https://www.instagram.com/kolyy_official/"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <img src={instaIcon} alt="instagram" />
-        </a>
-      </div>
-      <div>
-        <a
-          href="https://www.facebook.com/kolyyofficial/"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <img src={facebookIcon} alt="facebook" />
-        </a>
-      </div>
-      <div>
-        <a
-          href="https://www.linkedin.com/company/petki/"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <img src={linkedinIcon} alt="linkedin" />
-        </a>
-      </div>
-      <div>
-        <a
-          href="https://twitter.com/Kolyy_official"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <img src={twitterIcon} alt="twitter" />
-        </a>
-      </div>
+      <a
+        href="https://www.instagram.com/kolyy_official/"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <img src={instaIcon} alt="instagram" />
+      </a>
+      <a
+        href="https://www.facebook.com/kolyyofficial/"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <img src={facebookIcon} alt="facebook" />
+      </a>
+      <a
+        href="https://www.linkedin.com/company/petki/"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <img src={linkedinIcon} alt="linkedin" />
+      </a>
+      <a
+        href="https://twitter.com/Kolyy_official"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <img src={twitterIcon} alt="twitter" />
+      </a>
     </div>
   );
 };
