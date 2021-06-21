cbeecd702f35080d76e05dbba285c5c8ad0acae1
Pau Larrea <paularrea12@hotmail.com>
2021-06-21 11:52:18 +0200

    change mockup 0perrosperdidos

--- src/components/0perrosPerdidosComponents/app.jsx
+++ src/components/0perrosPerdidosComponents/app.jsx
@@ -9,7 +9,7 @@ import mockup from "../../images/app-zero-perros-perdidos.jpg";
 
 const App = () => {
   return (
-    <div className={container} style={{ backgroundColor: "#AFD0D0" }}>
+    <div className={container} style={{ backgroundColor: "#AFD0D0" }} >
       <div className={flex_container}>
         <div className={text}>
           <h2 data-sal="slide-up" data-sal-delay="100" data-sal-duration="1000">
