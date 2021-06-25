7feb8721923d0cab18d7d9d3cc06628c02f4b8a2
Pau Larrea <paularrea12@hotmail.com>
2021-06-25 12:15:39 +0200

    gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,30 +12,11 @@ module.exports = {
         trackingIds: [
           "G-HYQKXZ0WY9",
         ],
-      },
-      pluginConfig: {
-        head: true,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        id: "GTM-583PZMR",
-        includeInDevelopment: false,
-        defaultDataLayer: { platform: "gatsby" },
-  
-        // Specify optional GTM environment details.
-        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-        dataLayerName: "YOUR_DATA_LAYER_NAME",
-  
-        // Name of the event that is triggered
-        // on every Gatsby route change.
-        //
-        // Defaults to gatsby-route-change
-        routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
-        // Defaults to false
-        enableWebVitalsTracking: true,
+        pluginConfig: {
+          head: false,
+          respectDNT: true,
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
       },
     },
     `gatsby-transformer-remark`,
