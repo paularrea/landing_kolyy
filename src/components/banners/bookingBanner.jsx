5b7cb3de32e4b8f9885cce30c35546667326f188
Pau Larrea <paularrea12@hotmail.com>
2021-07-21 18:56:41 +0200

    banner booking modification and closing button

--- src/components/banners/bookingBanner.jsx
+++ src/components/banners/bookingBanner.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
 import MediaQuery from "react-responsive";
-import { banner_container, flex_container } from "./banners.module.scss";
+import {
+  banner_container,
+  flex_container,
+  close_banner,
+} from "./banners.module.scss";
 import collarsCollection from "../../images/coleccion-collar-perro-kolyy.png";
+import close_banner_img from "../../images/icons/X.png";
 
 const BookingBanner = () => {
+  const [closeBanner, setCloseBanner] = useState(false);
+
   return (
     <>
       <MediaQuery maxWidth={700}>
-        <div className={banner_container}>
+        <div
+          style={{ display: closeBanner && "none" }}
+          className={banner_container}
+        >
+          <button onClick={() => setCloseBanner(true)} className={close_banner}>
+            <img src={close_banner_img} alt="cerrar banner" />
+          </button>
           <h3>¡Apúntate a la lista para reservar tu collar!</h3>
           <div className={flex_container}>
             <img
@@ -20,14 +33,20 @@ const BookingBanner = () => {
             </Link>
           </div>
         </div>
-      </MediaQuery >
+      </MediaQuery>
       <MediaQuery minWidth={700}>
-      <div className={banner_container}>
+        <div
+          style={{ display: closeBanner && "none" }}
+          className={banner_container}
+        >
+          <button onClick={() => setCloseBanner(true)} className={close_banner}>
+            <img src={close_banner_img} alt="cerrar banner" />
+          </button>
           <img
             src={collarsCollection}
             alt="colección de collares para perros kolyy"
           />
-          <div style={{textAlign:'center'}}>
+          <div style={{ textAlign: "center" }}>
             <h3>¡Apúntate a la lista para reservar tu collar!</h3>
             <Link to="/reserva-collar-kolyy">
               <button>Apuntarme</button>
